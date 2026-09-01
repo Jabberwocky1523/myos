a166e96af11cae36dd7b3309308715124652d0c9
Jabberwocky1523 <439887070@qq.com>
2026-09-01 15:41:19 +0800

    modified:   .gitignore 	new file:   Makefile 	new file:   common.mk 	new file:   kernel.ld 	modified:   spec/modules/buffer-bitmap.yaml 	new file:   spec/readme.md 	new file:   src/kernel/arch/method.h 	new file:   src/kernel/arch/mod.c 	new file:   src/kernel/arch/mod.h 	new file:   src/kernel/arch/type.h 	new file:   src/kernel/boot/entry.S 	new file:   src/kernel/boot/start.c 	new file:   src/kernel/fs/bitmap.c 	new file:   src/kernel/fs/buf.c 	new file:   src/kernel/fs/fs.c 	new file:   src/kernel/fs/method.h 	new file:   src/kernel/fs/mod.h 	new file:   src/kernel/fs/type.h 	new file:   src/kernel/fs/virtio.c 	new file:   src/kernel/lib/method.h 	new file:   src/kernel/lib/print.c 	new file:   src/kernel/lib/type.h 	new file:   src/kernel/lib/uart.c 	new file:   src/kernel/lib/utils.c 	new file:   src/kernel/lock/method.h 	new file:   src/kernel/lock/sleeplock.c 	new file:   src/kernel/lock/spinlock.c 	new file:   src/kernel/lock/type.h 	new file:   src/kernel/main.c 	new file:   src/kernel/mem/kvm.c 	new file:   src/kernel/mem/method.h 	new file:   src/kernel/mem/mmap.c 	new file:   src/kernel/mem/pmem.c 	new file:   src/kernel/mem/type.h 	new file:   src/kernel/mem/uvm.c 	new file:   src/kernel/proc/method.h 	new file:   src/kernel/proc/proc.c 	new file:   src/kernel/proc/swtch.S 	new file:   src/kernel/proc/type.h 	new file:   src/kernel/syscall/method.h 	new file:   src/kernel/syscall/mod.h 	new file:   src/kernel/syscall/syscall.c 	new file:   src/kernel/syscall/sysfunc.c 	new file:   src/kernel/syscall/type.h 	new file:   src/kernel/trap/method.h 	new file:   src/kernel/trap/plic.c 	new file:   src/kernel/trap/timer.c 	new file:   src/kernel/trap/trampoline.S 	new file:   src/kernel/trap/trap.S 	new file:   src/kernel/trap/trap_kernel.c 	new file:   src/kernel/trap/trap_user.c 	new file:   src/kernel/trap/type.h 	new file:   src/mkfs/mkfs.c 	new file:   src/mkfs/mkfs.h 	new file:   src/user/initcode.c 	new file:   src/user/sys.h 	new file:   src/user/syscall_arch.h 	new file:   src/user/syscall_num.h

---
+++ src/kernel/mem/type.h
@@ -0,0 +1,79 @@
+#ifndef KERNEL_MEM_TYPE_H
+#define KERNEL_MEM_TYPE_H
+
+#include "../lib/type.h"
+#include "../lock/type.h"
+
+#define PAGE_SIZE 4096UL
+#define PGSIZE PAGE_SIZE
+#define PAGE_MASK (PAGE_SIZE - 1)
+#define PGROUNDUP(x) (((uint64)(x) + PAGE_MASK) & ~PAGE_MASK)
+#define PGROUNDDOWN(x) ((uint64)(x) & ~PAGE_MASK)
+
+#define KERNEL_BASE 0x80000000UL
+#define PHYSTOP 0x88000000UL
+#define UART0 0x10000000UL
+#define VIRTIO0 0x10001000UL
+#define CLINT 0x02000000UL
+#define CLINT_SIZE 0x00010000UL
+#define PLIC 0x0c000000UL
+#define PLIC_SIZE 0x04000000UL
+
+#define VA_MAX (1UL << 38)
+#define TRAMPOLINE (VA_MAX - PAGE_SIZE)
+#define TRAPFRAME (TRAMPOLINE - PAGE_SIZE)
+#define KSTACK(procid) (TRAPFRAME - (((uint64)(procid) + 1UL) * 2UL * PGSIZE))
+#define USER_BASE PAGE_SIZE
+#define USER_HEAP_BASE (USER_BASE + PAGE_SIZE)
+#define MMAP_END (TRAPFRAME - 16UL * 256UL * PAGE_SIZE)
+#define MMAP_BEGIN (MMAP_END - 64UL * 256UL * PAGE_SIZE)
+#define USER_STACK_BOTTOM MMAP_END
+#define USER_STACK_TOP TRAPFRAME
+#define USER_STACK (USER_STACK_TOP - PAGE_SIZE)
+#define MMAP_REGION_COUNT 256U
+#define NPROC 64U
+
+#define SATP_SV39 (8UL << 60)
+#define MAKE_SATP(pgtbl) (SATP_SV39 | ((uint64)(pgtbl) >> 12))
+#define PTE_V (1UL << 0)
+#define PTE_R (1UL << 1)
+#define PTE_W (1UL << 2)
+#define PTE_X (1UL << 3)
+#define PTE_U (1UL << 4)
+#define PTE_FLAGS(pte) ((pte) & 0x3ffUL)
+#define PTE_PERMS(pte) ((pte) & (PTE_V | PTE_R | PTE_W | PTE_X | PTE_U))
+#define PA_TO_PTE(pa) ((((uint64)(pa)) >> 12) << 10)
+#define PTE_TO_PA(pte) (((pte) >> 10) << 12)
+#define VA_TO_VPN(level, va) ((((uint64)(va)) >> (12 + 9 * (level))) & 0x1ff)
+
+// 检查一个PTE是否属于pgtbl
+#define PTE_CHECK(pte) (((pte) & (PTE_R | PTE_W | PTE_X)) == 0)
+
+typedef uint64 pte_t;
+typedef pte_t *pgtbl_t;
+
+typedef struct mmap_region
+{
+  uint64 begin;
+  uint64 end;
+  int perm;
+  uint32 index;
+  struct mmap_region *next;
+} mmap_region_t;
+
+typedef struct free_page
+{
+  struct free_page *next;
+} free_page_t;
+
+typedef struct pmem_region
+{
+  spinlock_t lock;
+  free_page_t *freelist;
+  uint64 begin;
+  uint64 end;
+  uint64 allocable;
+  const char *name;
+} pmem_region_t;
+
+#endif
---
+++ src/kernel/syscall/mod.h
@@ -0,0 +1,6 @@
+#ifndef KERNEL_SYSCALL_MOD_H
+#define KERNEL_SYSCALL_MOD_H
+
+#include "method.h"
+
+#endif
